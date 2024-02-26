b63b28bca6770cfab20cb6ef2d30561ea07345a3
Radhikakhatri972 <153940050+Radhikakhatri972@users.noreply.github.com>
2024-02-26 23:08:59 +0530

    Update Assesment2.cpp

--- Assesment2.cpp
+++ Assesment2.cpp
@@ -1,67 +1,62 @@
-#include<cstdlib>
-#include<ctime>
-#include<iostream>
+//Write a program to demonstrate a Rock Paper Scissor Game
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 using namespace std;
-int main(){
-    string name;
-    char playAgain;
-    int TotalRound,round=0;
-    int userChoice , computerChoice;
-    int userScore = 0 , computerScore = 0;
-    
-    cout<<"Enter your name: ";
-    cin>>name;
 
-    cout<<endl<<name<<" , how many rounds you want to play : ";
-    cin>>TotalRound;
+int userScore = 0;
+int computerScore = 0;
+
+void gameStart(int TotalRound, const string& name) {
 
     cout<<endl<<"Welcome to Stone Paper Scissor Game.\n";
-do{
-    cout << "\nRound: " << round+1 << " / "<<TotalRound<<endl;
 
-    cout<<endl<<"* "<<name<<"'s"<<" Score = "<<userScore<<endl;
-    cout<<"* "<<"computer"<<" Score = "<<computerScore<<endl;
+    for (int round = 0; round < TotalRound; ++round) {
 
-    cout <<endl<< "1) Rock\n2) Paper\n3) Scissors" << endl;
-    cout <<endl<< "-> Select your choice : ";
-    cin >> userChoice;
+        cout << "\nRound: " << round + 1 << " / " << TotalRound << endl;
 
-    computerChoice = rand() % 3 + 1;
+        cout << endl << "* " << name << "'s Score = " << userScore << endl;
+        cout << "* Computer Score = " << computerScore << endl;
 
-    cout<<endl<<name<<" choice is : "<<userChoice<<endl;
-    cout<<"Computer choice is : "<<computerChoice<<endl;
-    
-    if(userChoice==computerChoice){
-        cout<<"\nDraw!\n";
+        int userChoice, computerChoice;
+        cout <<endl<< "1) Rock\n2) Paper\n3) Scissors" << endl;
+        cout << endl << "-> Select your choice: ";
+        cin >> userChoice;
+
+        computerChoice = rand() % 3 + 1;
+
+        cout << endl << name << "'s choice is: " << userChoice << endl;
+        cout << "Computer's choice is: " << computerChoice << endl;
+
+        if (userChoice == computerChoice) {
+            cout << "\nDraw!\n";
+        }
+        else if ((userChoice == 1 && computerChoice == 3) ||
+                   (userChoice == 2 && computerChoice == 1) ||
+                   (userChoice == 3 && computerChoice == 2)) 
+                {
+                    cout << "\nCongratulations! " << name << " wins this round.\n" << endl;
+                    userScore++;
+                } 
+        else {
+                     cout << "\nOh! Computer won the round!\n" << endl;
+                     computerScore++;
+                }
     }
-    else if((userChoice == 1 & computerChoice == 3)|| 
-            (userChoice == 2 & computerChoice == 1) || 
-            (userChoice == 3 & computerChoice == 2))
-            {
-                cout<<"\nCongratulation! "<<name<<" win this round.\n"<<endl;
-                userScore++;
-            }
-            else{
-                cout<<"\nOh! Computer won the Game!\n"<<endl;
-                computerScore++;
-            }
-
-            round++;
-            
-            // cout << "\nRound: " << userScore + computerScore + 1 << "/3\n";
-            cout << "* "<<name<<"'s score = " << userScore << endl;
-            cout << "* Computer score = " << computerScore << endl;
-
-            if (round < TotalRound)
-            {   cout << "\nPress any key to continue...";
-                cin.ignore();
-                cin.get();
-            }
-    }
-
-            while (round < TotalRound);
-
-            cout << "\nGame is drawn.\n";
-    
 }
- 
+
+int main() {
+    srand(time(nullptr));
+    string name;
+    int TotalRound;
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << endl << name << ", how many rounds do you want to play: ";
+    cin >> TotalRound;
+
+    gameStart(TotalRound, name);
+
+    cout << "\nGame is drawn.\n";
+    return 0;
+}
